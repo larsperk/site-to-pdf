6fcda9170efa281737eec688dda8c819f09bcd0f
larsperk <lars@larsperkins.com>
2023-10-03 12:22:48 -0400

    minor refactor to make switching pdf/txt mode clearer

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ from bs4 import BeautifulSoup
 import pdfkit
 import os
 
+SITE_ROOT = "https://reverb.hardrockhotels.com/"
+OUTPUT_FILE = "reverb_hard_rock_blob.txt"
+MODE = "TEXT-BLOB"
+
+excluded_url_strings = [
+    "drive.google.com",
+    "feeds",
+    "audio",
+    "podcast",
+    ".jpeg",
+    ".jpg"
+]
 
 class WebsiteScraper:
     def __init__(self, start_url):
@@ -24,7 +36,7 @@ class WebsiteScraper:
 
     def save_as_text(self, url, text):
 
-        if "drive.google.com" in url or "feeds" in url or "audio" in url or "podcast" in url:
+        if any([excluded_url_string in url for excluded_url_string in excluded_url_strings]):
             result = False
             print(f'ignoring link {url}')
 
@@ -34,7 +46,7 @@ class WebsiteScraper:
                 path = os.path.join("txts", filename)
                 text = " " + text + " "
                 # text_file = open(path, "w")
-                path = "txts/upenn-blob.txt"
+                path = "txts/" + OUTPUT_FILE
                 text_file = open(path, "a")
                 result = text_file.write(text)
                 print(f"Saved {url} as {path}")
@@ -47,8 +59,11 @@ class WebsiteScraper:
         return result
 
     def scrape(self, url):
-        if "drive.google.com" not in url and "feeds" not in url and "audio" not in url and "podcast" not in url:
+        if any([excluded_url_string in url for excluded_url_string in excluded_url_strings]):
+            result = False
+            print(f'not scraping {url}')
 
+        else:
             url = "https://" + url.split("//")[-1]
             if url in self.visited_urls:
                 return
@@ -61,10 +76,11 @@ class WebsiteScraper:
 
             soup = BeautifulSoup(response.text, 'html.parser')
             text = soup.getText(separator=' ')
-            text = soup.getText()
 
-            result = self.save_as_text(url, text)
-            # self.save_as_pdf(url)
+            if MODE == "TEXT-BLOB":
+                result = self.save_as_text(url, text)
+            elif MODE == "PDFS":
+                self.save_as_pdf(url)
 
             links = [a['href'] for a in soup.find_all('a', href=True)]
             links = [link for link in links if "mailto" not in link]
@@ -77,18 +93,22 @@ class WebsiteScraper:
                 # Check if the link is internal to the website
                 if self.is_internal(link) and link not in self.visited_urls:
                     self.scrape(link)
-        else:
-            print(f'not scraping {url}')
 
 
 if __name__ == "__main__":
-    scraper = WebsiteScraper("https://www.library.upenn.edu/")
+    scraper = WebsiteScraper(SITE_ROOT)
 
     # Create a directory to save the PDFs
-    if not os.path.exists("pdfs"):
-        os.makedirs("pdfs")
 
-    if not os.path.exists("txts"):
-        os.makedirs("txts")
+    if MODE == "PDFS":
+        if not os.path.exists("pdfs"):
+            os.makedirs("pdfs")
+        scraper.scrape(scraper.start_url)
 
-    scraper.scrape(scraper.start_url)
+    elif MODE == "TEXT-BLOB":
+        if not os.path.exists("txts"):
+            os.makedirs("txts")
+        scraper.scrape(scraper.start_url)
+
+    else:
+        printf(f"Invalid scrape mode{MODE}")
